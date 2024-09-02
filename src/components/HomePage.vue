01d6ffca2d603b7aa48c6a0555f0d19334d70cb9
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-02 17:13:56 +0530

    added the front page elements . will add the gsap component next

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,13 +3,34 @@
     <div class="home-title">
         Hello
     </div>
+    <div class="home-name">
+        Im <span>Sandeep Sreekumar</span>
+    </div>
+    <div class="home-about">
+        Im a <span id="typewriter-about"></span>
+    </div>
   </div>
 </template>
 
 <script>
 import '../Style/homepage.css'
+import Typewriter from 'typewriter-effect/dist/core';
 export default {
-    name:'HomePage'
+    name:'HomePage',
+    data(){
+        return{
+
+        }
+    },
+    mounted(){
+       const typeWiterAbout = new Typewriter ('#typewriter-about',{
+        strings:['Front-End Developer ','Back-End Developer '],
+        autoStart:true,
+        loop:true,
+        delay:75
+       })
+       typeWiterAbout.start()
+    }
 }
 </script>
 
