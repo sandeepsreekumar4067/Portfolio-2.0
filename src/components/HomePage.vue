4236c425261fd3187fbdab2b305510547179358c
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-02 16:38:07 +0530

    initial commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-container">
+    <div class="gradient1"></div>
+    <div class="gradient2"></div>
+  </div>
+</template>
+
+<script>
+import '../Style/homepage.css'
+export default {
+    name:'HomePage'
+}
+</script>
+
+<style>
+
+</style>
