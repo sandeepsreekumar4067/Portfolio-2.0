08c2545bda009ae7e000cccba7f975091e0cae6b
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-02 20:02:59 +0530

    making the second page

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <HomePage/>
+  <SecondPage/>
 </template>
 
 <script>
 import './Style/app.css'
 import HomePage from './components/HomePage.vue';
+import SecondPage from './components/SecondPage.vue';
 
 
 export default {
   name: 'App',
   components: {
-    HomePage
+    HomePage,
+    SecondPage
   }
 }
 </script>
---
+++ src/components/SecondPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="secondPage-container">
+        helo world
+    </div>
+</template>
+
+<script>
+import '../Style/secondpage.css'
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
