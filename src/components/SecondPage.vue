b2ef848b132ebf413382bcc6e94e7d0f14c075e4
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-05 19:56:01 +0530

    finally something is working in the second container

--- src/components/SecondPage.vue
+++ src/components/SecondPage.vue
@@ -1,32 +1,31 @@
 <template>
   <div class="secondPage-container" id="sc-main">
     <div class="secondPage-display-container" id="sc-display">
-
+      <div class="secondPage-circle-container" id="sc-circle"></div>
     </div>
   </div>
 </template>
 
 <script>
-import '../Style/secondpage.css'
-import {gsap} from 'gsap'
+import "../Style/secondpage.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 export default {
-    mounted(){
-        gsap.from('#sc-display',{
-            scrollTrigger:{
-                trigger:'#sc-display',
-                start:'top 0%',
-                pin:true,
-                toggleActions:'play pause reverse replay'
-            },
-            ease:'bounce.inOut',
-            borderRadius:'50%',
-            height:'10%',
-            width:'10%'
-        })
-    }
-}
+  mounted() {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to("#sc-circle", {
+      y: "34rem", // Moves the circle down 20rem
+      ease: "bounce.out", // Bounce effect on fall
+      duration: 2, // Total time for the fall and bounce
+      scrollTrigger: {
+        trigger: "#sc-main", // When the top of secondPage-container hits the top of the viewport
+        start: "top 10%", // Start when the top of sc-main hits the top of the viewport
+        toggleActions: "play none none none", // Only play once
+      },
+    });
+  },
+};
 </script>
 
 <style>
-
 </style>
