832f7420812caa7eda1c4e2288c5b5a30c706de3
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-05 19:32:34 +0530

    bugs will try to fix

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <HomePage/>
+  <SecondPage/>
   <!-- <ThirdPage/> -->
 </template>
 
 <script>
 import './Style/app.css'
 import HomePage from './components/HomePage.vue';
+import SecondPage from './components/SecondPage.vue';
 // import ThirdPage from './components/ThirdPage.vue';
 
 
@@ -13,6 +15,7 @@ export default {
   name: 'App',
   components: {
     HomePage,
+    SecondPage,
     // ThirdPage
   }
 }
---
+++ src/components/SecondPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="secondPage-container" id="sc-main">
+    <div class="secondPage-display-container" id="sc-display">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import '../Style/secondpage.css'
+import {gsap} from 'gsap'
+export default {
+    mounted(){
+        gsap.from('#sc-display',{
+            scrollTrigger:{
+                trigger:'#sc-display',
+                start:'top 0%',
+                pin:true,
+                toggleActions:'play pause reverse replay'
+            },
+            ease:'bounce.inOut',
+            borderRadius:'50%',
+            height:'10%',
+            width:'10%'
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
