dbce732d88abd60a7bdd253dd4214180b78287e5
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-11 23:30:27 +0530

    made the front page

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="homepage-container">
     <div class="left-container">
-
+      <div class="left-about-container">
+        <div class="left-title">Hi, I am</div>
+        <div>
+          <div class="left-name">Sandeep Sreekumar</div>
+          <div class="left-desc">Frontend Developer / Full Stack</div>
+        </div>
+      </div>
+      <div class="left-button-container">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
-    <div class="right-container">
-
-    </div>
+    <div class="right-container"></div>
   </div>
 </template>
 
 <script>
-import '../Style/homepage.css'
-export default {
-
-}
+import "../Style/homepage.css";
+export default {};
 </script>
 
 <style>
-
 </style>
