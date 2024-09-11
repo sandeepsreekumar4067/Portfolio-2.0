b7b34114317727a12be2379a65a95cf763bf0e7b
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-12 00:58:29 +0530

    fixed the bugs . about to make it responsive

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,55 +1,52 @@
 <template>
   <div class="homepage-container">
     <div class="wrapper">
-     <div class="left-container">
-      <div class="left-about-container">
-        <div class="left-title">Hi I'm</div>
+      <div class="left-container">
+        <div class="left-about-container">
+          <div class="left-title">Hi I'm</div>
           <div>
             <div class="left-name">Sandeep Sreekumar</div>
             <div class="left-desc">Frontend Developer / Full Stack</div>
           </div>
+        </div>
+        <div class="left-button-container">
+          <span>
+            <i class="bi bi-github"></i>
+          </span>
+          <span>
+            <i class="bi bi-linkedin"></i>
+          </span>
+          <span>
+            <i class="bi bi-instagram"></i>
+          </span>
+          <span>
+            <i class="bi bi-twitter-x"></i>
+          </span>
+        </div>
       </div>
-      <div class="left-button-container">
-        <span>
-          <i class="bi bi-github"></i>
-        </span>
-        <span>
-          <i class="bi bi-linkedin"></i>
-        </span>
-        <span>
-          <i class="bi bi-instagram"></i>
-        </span>
-        <span>
-          <i class="bi bi-twitter-x"></i>
-        </span>
+      <div class="right-container">
+        <img :src="homeImage" alt="" />
       </div>
-     </div>
-     <div class="right-container">
-
-     </div>
+    </div>
+    <div class="bottom-container">
+      <span>Hey Guys</span>
+      I'm a Full Stack Developer with a focus on web and mobile applications. I
+      specialize in Frontend Development using HTML, CSS, JavaScript, React, and
+      Vue.js, and also work on the backend with Python and FastAPI. Currently,
+      I'm pursuing a B.Tech in Computer Science and Engineering at VJCET. I'm
+      passionate about building intuitive and impactful digital solutions
     </div>
   </div>
 </template>
 
 <script>
 import "../Style/homepage.css";
-export default {};
+import homeImage from "../assets/homeImage2.png";
+export default {
+  data() {
+    return {
+      homeImage: homeImage,
+    };
+  },
+};
 </script>
-
-<style>
-</style>
-<!-- <div class="left-container">
-  <div class="left-about-container">
-    <div class="left-title">Hi, I am</div>
-      <div>
-        <div class="left-name">Sandeep Sreekumar</div>
-        <div class="left-desc">Frontend Developer / Full Stack</div>
-      </div>
-    </div>
-  <div class="left-button-container">
-    <span></span>
-    <span></span>
-    <span></span>
-  </div>
-</div>
-<div class="right-container"></div> -->
