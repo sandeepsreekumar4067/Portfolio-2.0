b1ffceb9a9f74f552d90a521bfda1cb2b640492c
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-15 20:46:54 +0530

    making the title gsap

--- src/components/ThirdPage.vue
+++ src/components/ThirdPage.vue
@@ -1,17 +1,11 @@
 <template>
   <div class="third-page-container" id="tmain">
     <div class="third-page-components" id="components">
-      <div class="circle">My Skills</div>
-      <div class="skill-container">
-        <div class="rows">
-          <span>
-
-          </span>
-        </div>
-        <div class="rows"></div>
-        <div class="rows"></div>
-        <div class="rows"></div>
-      </div>
+      <div class="skill-title">My Skills</div>
+      <div class="skill-container1"></div>
+      <div class="skill-container2"></div>
+      <div class="skill-container3"></div>
+      <div class="skill-container4"></div>
     </div>
   </div>
 </template>
@@ -22,59 +16,58 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import c from "../assets/languageIcons/c.png";
 import cpp from "../assets/languageIcons/c++.png";
-import python from "../assets/languageIcons/python.png"
-import java from "../assets/languageIcons/java.png"
-import js from "../assets/languageIcons/js.png"
-import dart from "../assets/languageIcons/dart.png"
-import experss from "../assets/languageIcons/expressjs.png"
-import vue from "../assets/languageIcons/vueJs.png"
-import firebase from "../assets/languageIcons/fireabase.png"
-import flutter from "../assets/languageIcons/flutter.png"
-import react from "../assets/languageIcons/react.png"
-import node from "../assets/languageIcons/nodejs.png"
-import mongo from "../assets/languageIcons/mongoDb.png"
-import spline from "../assets/languageIcons/spline.png"
-import ts from "../assets/languageIcons/typescript.png"
-import mySql from "../assets/languageIcons/mySql.png"
-import html from "../assets/languageIcons/html.png"
-import css from "../assets/languageIcons/css.png"
-import github from "../assets/languageIcons/github.png"
+import python from "../assets/languageIcons/python.png";
+import java from "../assets/languageIcons/java.png";
+import js from "../assets/languageIcons/js.png";
+import dart from "../assets/languageIcons/dart.png";
+import experss from "../assets/languageIcons/expressjs.png";
+import vue from "../assets/languageIcons/vueJs.png";
+import firebase from "../assets/languageIcons/fireabase.png";
+import flutter from "../assets/languageIcons/flutter.png";
+import react from "../assets/languageIcons/react.png";
+import node from "../assets/languageIcons/nodejs.png";
+import mongo from "../assets/languageIcons/mongoDb.png";
+import spline from "../assets/languageIcons/spline.png";
+import ts from "../assets/languageIcons/typescript.png";
+import mySql from "../assets/languageIcons/mySql.png";
+import html from "../assets/languageIcons/html.png";
+import css from "../assets/languageIcons/css.png";
+import github from "../assets/languageIcons/github.png";
 
 export default {
-  data(){
-    return{
-      c:c,
-      cpp:cpp,
-      python:python,
-      java:java,
-      js:js,
-      dart:dart,
-      experss:experss,
-      vue:vue,
-      firebase:firebase,
-      flutter:flutter,
-      react:react,
-      node:node,
-      mongo:mongo,
-      spline:spline,
-      ts:ts,
-      mySql:mySql,
-      html:html,
-      css:css,
-      github:github,
-      pl:[c,cpp,python,java,js],
-      wd:[html,css,react,vue,node,experss],
-      md:[flutter],
-      db:[firebase,mySql,mongo],
-      tools:[github]
-    }
+  data() {
+    return {
+      c: c,
+      cpp: cpp,
+      python: python,
+      java: java,
+      js: js,
+      dart: dart,
+      experss: experss,
+      vue: vue,
+      firebase: firebase,
+      flutter: flutter,
+      react: react,
+      node: node,
+      mongo: mongo,
+      spline: spline,
+      ts: ts,
+      mySql: mySql,
+      html: html,
+      css: css,
+      github: github,
+      pl: [c, cpp, python, java, js],
+      wd: [html, css, react, vue, node, experss],
+      md: [flutter],
+      db: [firebase, mySql, mongo],
+      tools: [github],
+    };
   },
   mounted() {
     gsap.registerPlugin(ScrollTrigger);
     // const timeline = gsap.timeline({
     //   scrollTrigger: {
     //     trigger: "#components",
-    //     markers: true,
     //     start: "top 10%",
     //     pin: true,
     //     scrub: 1,
@@ -83,16 +76,41 @@ export default {
     //     toggleActions: 'play reverse play reverse', // Ensures the animation reverses when scrolling back up
     //   },
     // });
-    // timeline.from(".circle", {
+    // timeline.from(".skill-title", {
     //   opacity: 0,
     //   x:-300
     // });
-    // timeline.from('.skill-container',{
-    //   width:'2rem',
-    //   height:'2rem',
-    //   opacity:0,    
-    //   snap:1
-    // })
+     gsap.timeline({
+      scrollTrigger: {
+        trigger: ".skill-title",
+        pin: true,
+        start: "top 8%",
+        markers:true,
+        endTrigger: "skill-title",
+      },
+    });
+    gsap.from('.skill-title',{
+      scrollTrigger:{
+        trigger:'.skill-title',
+        start:'top 50%',
+        scrub:1,
+        end:'top 8%'
+      },
+      x:-500,
+      duration:1.8,
+      opacity:0
+    })
+    // gsap.from(".skill-title", {
+    //   scrollTrigger: {
+    //     trigger: "#components",
+    //     start: "top 20%",
+    //     scrub: 1,
+    //     pin: true,
+    //     pinSpacing: false,
+    //   },
+    //   x: -300,
+    //   opacity: 0,
+    // });
   },
 };
 </script>
