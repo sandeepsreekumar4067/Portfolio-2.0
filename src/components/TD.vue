eb9962917ef02c2e1a313f0fee7b894e84229053
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-15 23:45:59 +0530

    doing my way

--- src/components/TD.vue
+++ src/components/TD.vue
@@ -1,20 +1,37 @@
 <template>
   <div class="third-page-container">
-    <div class="skill-title">
-        My Skills
+    <div class="skill-title">My Skills</div>
+    <div class="skills-timeline">
+      <div class="line"></div>
+      <!-- <div
+        v-for="(skill, index) in skills"
+        :key="index"
+        class="skill-box"
+        :class="{ left: index % 2 === 0, right: index % 2 !== 0 }"
+      >
+        <img :src="c" alt="" class="skill-icon" />
+        <div class="skill-name">{{ skill.name }}</div>
+      </div> -->
+      <div v-for="(skill,key) in skills" :key="key" class="skill-box" >
+        
+      </div>
     </div>
   </div>
 </template>
+  
 
-<script>
-import '../Style/td.css'
+  <script>
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "../Style/td.css";
+// Importing icons
 import c from "../assets/languageIcons/c.png";
 import cpp from "../assets/languageIcons/c++.png";
 import python from "../assets/languageIcons/python.png";
 import java from "../assets/languageIcons/java.png";
 import js from "../assets/languageIcons/js.png";
 import dart from "../assets/languageIcons/dart.png";
-import experss from "../assets/languageIcons/expressjs.png";
+import express from "../assets/languageIcons/expressjs.png";
 import vue from "../assets/languageIcons/vueJs.png";
 import firebase from "../assets/languageIcons/fireabase.png";
 import flutter from "../assets/languageIcons/flutter.png";
@@ -26,19 +43,18 @@ import ts from "../assets/languageIcons/typescript.png";
 import mySql from "../assets/languageIcons/mySql.png";
 import html from "../assets/languageIcons/html.png";
 import css from "../assets/languageIcons/css.png";
-import github from "../assets/languageIcons/github.png";
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import github from '../assets/languageIcons/github.png'
 export default {
-    data() {
+  data() {
     return {
+      // Skills array with names and icons
       c: c,
       cpp: cpp,
       python: python,
       java: java,
       js: js,
       dart: dart,
-      experss: experss,
+      experss: express,
       vue: vue,
       firebase: firebase,
       flutter: flutter,
@@ -50,30 +66,58 @@ export default {
       mySql: mySql,
       html: html,
       css: css,
-      github: github,
-      pl: [c, cpp, python, java, js],
-      wd: [html, css, react, vue, node, experss],
-      md: [flutter],
-      db: [firebase, mySql, mongo],
-      tools: [github],
+      github:github,
+      skills: [
+        { icon: c, name: "C" },
+        { icon: cpp, name: "C++" },
+        { icon: python, name: "Python" },
+        { icon: java, name: "Java" },
+        { icon: js, name: "JavaScript" },
+        { icon: dart, name: "Dart" },
+        { icon: express, name: "Express.js" },
+        { icon: vue, name: "Vue.js" },
+        { icon: firebase, name: "Firebase" },
+        { icon: flutter, name: "Flutter" },
+        { icon: react, name: "React" },
+        { icon: node, name: "Node.js" },
+        { icon: mongo, name: "MongoDB" },
+        { icon: spline, name: "Spline" },
+        { icon: ts, name: "TypeScript" },
+        { icon: mySql, name: "MySQL" },
+        { icon: html, name: "HTML" },
+        { icon: css, name: "CSS" },
+      ],
     };
   },
-    mounted(){
-        gsap.registerPlugin(ScrollTrigger)
-        gsap.from('.skill-title',{
-            scrollTrigger:{
-                trigger:'.skill-title',
-                start:'top 70%',
-                end:'top 50%',
-                scrub:1
-            },
-            x:-300,
-            opacity:0
-        })
-    }
-}
+  mounted() {
+    gsap.registerPlugin(ScrollTrigger);
+    // Animate each skill box to pop in
+    // gsap.utils.toArray(".skill-box").forEach((box, index) => {
+    //   gsap.from(box, {
+    //     scrollTrigger: {
+    //       trigger: box,
+    //       start: "top 80%",
+    //       end: "top 60%",
+    //       scrub: 1,
+    //     },
+    //     x: index % 2 === 0 ? -200 : 200, // Slide from left or right
+    //     opacity: 0,
+    //     duration: 1,
+    //   });
+    // });
+
+    // // Animate the vertical line
+    // gsap.from(".line", {
+    //   scrollTrigger: {
+    //     trigger: ".skills-timeline",
+    //     start: "top 30%",
+    //     end: "bottom 60%",
+    //     scrub: 1,
+    //   },
+    //   height: 0,
+    //   duration: 1,
+    // });
+  },
+};
 </script>
-
-<style>
-
-</style>
+  
