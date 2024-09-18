df5b5a3a5e8ea4ace3b2e7fbedd8502a3d149973
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-18 18:42:21 +0530

    made the entry animation but bugs in the exit

--- src/components/FourthPage.vue
+++ src/components/FourthPage.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="fourth-page-container">
-    helo
+    <span></span><span></span><span></span><span></span>
   </div>
 </template>
 
 <script>
 import '../Style/fourthpage.css'
+import {gsap} from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 export default {
-
+    mounted(){
+        gsap.registerPlugin(ScrollTrigger)
+        gsap.utils.toArray('.fourth-page-container span').forEach((span)=>{
+            gsap.from(span,{
+                scrollTrigger:{
+                    trigger:span,
+                    start:'top 90%',
+                    scrub:1,
+                    end:'center center'
+                },
+                scale:0
+            },)
+        })
+   
+    }
 }
 </script>
 
-<style>
-
-</style>
