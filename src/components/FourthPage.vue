57027b8dc03627c01721a0ebfbf5ba6772269ab1
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-18 20:43:51 +0530

    making another way for the project container

--- src/components/FourthPage.vue
+++ src/components/FourthPage.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="fourth-page-container">
     <div class="project-container">
-      <video autoplay muted loop>
-        <source :src="neonVideo" type="video/mp4">
-      </video>
+      <img :src="neonImg" alt="">
+      <div class="cover-container">
+        
+      </div>
     </div>
   </div>
 </template>
@@ -12,32 +13,31 @@
 import "../Style/fourthpage.css";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import neonVideo from '../assets/videos/neon abstract 1.mp4'
+import neonImg from "../assets/project images/img1 (1).jpg";
 export default {
-
-    data(){
-        return{
-            neonVideo:neonVideo
-        }
-    },
+  data() {
+    return {
+      neonImg: neonImg,
+    };
+  },
   mounted() {
     gsap.registerPlugin(ScrollTrigger);
-    // gsap.utils.toArray('.fourth-page-container span').forEach((span)=>{
-    //     const tl = gsap.timeline({
-    //         scrollTrigger:{
-    //             trigger:span,
-    //             start:'top 90%',
-    //             end:'top 5%',
-    //             scrub:1
-    //         }
-    //     })
-    //     tl.from(span,{
-    //         scale:0
-    //     })
-    //     tl.to(span,{
-    //         scale:0
-    //     })
-    // })
+    gsap.utils.toArray('.project-container').forEach((span)=>{
+        const tl = gsap.timeline({
+            scrollTrigger:{
+                trigger:span,
+                start:'top 90%',
+                end:'top 5%',
+                scrub:1
+            }
+        })
+        tl.from(span,{
+            scale:0
+        })
+        tl.to(span,{
+            scale:0
+        })
+    })
   },
 };
 </script>
