fe8ffc389ffb8d3e6af73744e6d0e0cceb44ab8b
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-23 11:39:23 +0530

    made the last page/about to make it responsive

--- src/components/LastPage.vue
+++ src/components/LastPage.vue
@@ -11,16 +11,46 @@
             You can also find me On
         </div>
         <div class="social-media-components">
-
+            <span>
+                <img :src="instagram" alt="">
+                <p>Instagram</p>
+            </span>
+            <span>
+                <img :src="linkedin" alt="">
+                <p>Linkedin</p>
+            </span>
+            <span>
+                <img :src="github" alt="">
+                <p>Github</p>
+            </span>
+            <span>
+                <img :src="twitterX" alt="">
+                <p>X</p>
+            </span>
+        </div>
+        <div class="end-text">
+            Hit me up on my Social Media <br>
+            Lets create innovative Projects
         </div>
     </div>
   </div>
 </template>
 
 <script>
+import instagram from '../assets/socialmeda/instagram.svg'
+import twitterX from '../assets/socialmeda/twitter-x.svg'
+import linkedin from '../assets/socialmeda/linkedin-icon-2.svg'
+import github from '../assets/socialmeda/github.svg'
 import '../Style/lastpage.css'
 export default {
-
+    data(){
+        return{
+            instagram:instagram,
+            twitterX:twitterX,
+            linkedin:linkedin,
+            github:github
+        }
+    }
 }
 </script>
 
