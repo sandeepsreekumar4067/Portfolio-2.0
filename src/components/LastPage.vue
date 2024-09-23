2a900c33f0d2dc7fc462540afbe610fbf40f095d
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-23 11:59:19 +0530

    project 90% done

--- src/components/LastPage.vue
+++ src/components/LastPage.vue
@@ -3,11 +3,11 @@
     <div class="contact-title">
         Contact
     </div>
-    <div class="resume">
+    <div class="resume" id="common">
         Download my resume <span>Here</span>
     </div>
     <div class="social-media">
-        <div class="social-title">
+        <div class="social-title" id="common">
             You can also find me On
         </div>
         <div class="social-media-components">
@@ -28,10 +28,10 @@
                 <p>X</p>
             </span>
         </div>
-        <div class="end-text">
-            Hit me up on my Social Media <br>
-            Lets create innovative Projects
-        </div>
+            <div class="end-text">
+                Hit me up on my Social Media <br>
+                Lets create innovative Projects
+            </div>
     </div>
   </div>
 </template>
@@ -42,6 +42,8 @@ import twitterX from '../assets/socialmeda/twitter-x.svg'
 import linkedin from '../assets/socialmeda/linkedin-icon-2.svg'
 import github from '../assets/socialmeda/github.svg'
 import '../Style/lastpage.css'
+import {gsap} from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 export default {
     data(){
         return{
@@ -50,6 +52,49 @@ export default {
             linkedin:linkedin,
             github:github
         }
+    },
+    mounted(){
+        gsap.registerPlugin(ScrollTrigger)
+        gsap.from('.contact-title',{
+            scrollTrigger:{
+                trigger:'.contact-title',
+                start:'top 50%',
+            },
+            y:-100,
+            opacity:0,
+            ease:'bounce.out',
+            duration:1.2
+        })
+        gsap.from('#common',{
+            scrollTrigger:{
+                trigger:'#common',
+                start:'top 50%',
+            },
+            y:100,
+            opacity:0,
+            stagger:0.3,
+            duration:1.2
+        })
+        gsap.from('.social-media-components span',{
+            scrollTrigger:{
+                trigger:'.social-media-components',
+                start:'top 50%',
+            },
+            delay:1,
+            scale:0,
+            stagger:0.4,
+            ease:'bounce.out',
+            duration:1
+        })
+        gsap.from('.end-text',{
+            scrollTrigger:{
+                trigger:'.end-text',
+                start:'top 90%',
+            },
+            x:100,
+            opacity:0,
+            duration:0.9
+        })
     }
 }
 </script>
