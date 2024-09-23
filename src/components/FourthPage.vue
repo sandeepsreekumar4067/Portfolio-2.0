fa1c4644e106aad54da1e5a7682d4aef0a0a67fd
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-09-23 08:58:16 +0530

    made the fourth page another way

--- src/components/FourthPage.vue
+++ src/components/FourthPage.vue
@@ -1,22 +1,14 @@
 <template>
-  <div class="fourth-page-container">
-    <div class="project-container">
-      <img :src="neonImg" alt="" />
-      <div class="cover-container">
-        <div class="project-title">Lex Ai (backend)</div>
-        <div class="project-content">
-          <div class="project-left">project Link</div>
-          <div class="project-right">
-            This project is the backend for an AI legal consultant using
-            LangChain technology to control a Large Language Model (LLM). It
-            includes various components like `casualBot.py` and `lawyerBot.py`
-            to handle different types of user interactions, potentially offering
-            legal advice or information. The use of LangChain suggests advanced
-            AI capabilities for managing and interpreting complex legal queries,
-            while Python scripts manage tasks like data processing, training,
-            and interactions with the LLM.
-          </div>
+  <div class="fourth-wrapper" id="fmain">
+    <div class="fourth-page-container" id="fcomp">
+      <div class="background-image">
+        <img :src="mountainImage" alt="" />
+      </div>
+      <div class="fourth-page-components">
+        <div class="fourth-page-title">
+          <span>Projects</span>
         </div>
+        <div class="project-components"></div>
       </div>
     </div>
   </div>
@@ -24,34 +16,34 @@
 
 <script>
 import "../Style/fourthpage.css";
+import mountainImage from "../assets/background/mountainImage.jpg";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import neonImg from "../assets/project images/img1 (1).jpg";
 export default {
   data() {
     return {
-      neonImg: neonImg,
+      mountainImage: mountainImage,
     };
   },
   mounted() {
     gsap.registerPlugin(ScrollTrigger);
-    gsap.utils.toArray(".project-container").forEach((span) => {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: span,
-          start: "top 90%",
-          end: "top 5%",
-          scrub: 1,
-        },
-      });
-      tl.from(span, {
-        scale: 0,
-      });
-      tl.to(span, {
-        scale: 0,
-      });
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#fcomp",
+        start: "top top",
+        pin: true,
+        endTrigger:'#fmain',
+        end: "bottom bottom",
+        scrub: 1,
+      },
+    });
+    tl.from(".fourth-page-title", {
+      x:-100,
+      opacity:0
     });
   },
 };
 </script>
 
+<style>
+</style>
